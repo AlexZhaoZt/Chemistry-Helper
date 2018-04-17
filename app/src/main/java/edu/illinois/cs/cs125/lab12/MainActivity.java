022f71d342053a59e2cac86ea4238b4b1a5ac0e3
Geoffrey Challen <challen@illinois.edu>
2018-04-16 22:03:57 -0500

    Lab 12 starter code.

---
+++ app/src/main/java/edu/illinois/cs/cs125/lab12/MainActivity.java
@@ -0,0 +1,77 @@
+package edu.illinois.cs.cs125.lab12;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+/**
+ * Main class for our UI design lab.
+ */
+public final class MainActivity extends AppCompatActivity {
+    /** Default logging tag for messages from the main activity. */
+    private static final String TAG = "Lab12:Main";
+
+    /** Request queue for our API requests. */
+    private static RequestQueue requestQueue;
+
+    /**
+     * Run when this activity comes to the foreground.
+     *
+     * @param savedInstanceState unused
+     */
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set up the queue for our API requests
+        requestQueue = Volley.newRequestQueue(this);
+
+        setContentView(R.layout.activity_main);
+
+        startAPICall();
+    }
+
+    /**
+     * Run when this activity is no longer visible.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    /**
+     * Make a call to the weather API.
+     */
+    void startAPICall() {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "http://samples.openweathermap.org/data/2.5/weather?zip=61820,us&appid="
+                            + BuildConfig.API_KEY,
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            Log.d(TAG, response.toString());
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(final VolleyError error) {
+                            Log.e(TAG, error.toString());
+                        }
+                    });
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
